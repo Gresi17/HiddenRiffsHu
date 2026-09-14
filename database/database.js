db544f035f9d59a5dd54c658c35b7b49485abcf3
Gresi17 <dominikgrestyak5@gmail.com>
2026-09-14 19:53:12 +0200

    09.14

--- database/database.js
+++ database/database.js
@@ -1,10 +1,19 @@
-import mysql from "mysql2";
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
-    password: "Admin123",
-    database: "database"
+    password: "HiddenRiffs234",
+    database: "hiddenriffs",
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("Hiba a MySQL kapcsolódás során: ", err);
+        return;
+    }
+    console.log("Sikeres MySQL kapcsolódás!");
 });
 
 module.exports = connection;
